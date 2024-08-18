1373a5e39587536d1391c83cd195d03a098e6af0
Yoav TC <yoavtc2004@gmail.com>
2024-08-18 07:43:19 +0300

    Added UI bar logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,60 @@
 using UnityEngine;
+using UnityEngine.UI;
+using NaughtyAttributes;
 
 public class GameManager : Singleton<GameManager>
 {
+	[Header("Ship Values")]
+	[SerializeField] private int shipWealth;
+	[SerializeField] private int shipWeight;
+	
+	[Header("Information")]
+	[ProgressBar("Disposed Money", "shipWealth", EColor.Yellow)]
 	[SerializeField] private int disposedMoney;
+	[ProgressBar("Disposed Weight", "shipWeight", EColor.Green)]
 	[SerializeField] private int disposedWeight;
+	
+	[Header("Components")]
+	[SerializeField] private Slider moneyBar;
+	[SerializeField] private Slider weightBar;
+	
+	private void Start()
+	{
+		GetWeightAndMoneyValues();
+		
+		moneyBar.maxValue = shipWealth;
+		moneyBar.value = shipWealth;
+		
+		weightBar.maxValue = shipWeight;
+		weightBar.value = shipWeight;
+	}
 	
 	public void ObjectDisposed(DisposableObject disposedObject) 
 	{
 		disposedMoney += disposedObject._price;
 		disposedWeight += disposedObject._weight;
+		
+		UpdateBars();
+	}
+	
+	private void UpdateBars() 
+	{
+		moneyBar.value = moneyBar.maxValue - disposedMoney;
+		weightBar.value = weightBar.maxValue - disposedWeight;
+	}
+	
+	private void GetWeightAndMoneyValues() 
+	{
+		DisposableObject[] disposableObjects = FindObjectsByType<DisposableObject>(FindObjectsSortMode.None);
+		
+		shipWealth = 0;
+		shipWeight = 0;
+		
+		for (int i = 0; i < disposableObjects.Length; i++) 
+		{
+			Debug.Log("Adding " + disposableObjects[i] + " To the sum of ship wealth & weight!", disposableObjects[i]);
+			shipWealth += disposableObjects[i]._price;
+			shipWeight += disposableObjects[i]._weight;
+		}
 	}
 }
